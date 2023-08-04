062d899122c3c36604e63b0acbefffdca2a9a8f3
ShilpaMRao <shilpamrao@outlook.com>
2023-08-03 23:28:05 -0700

    Navigation pages

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LittleLemonHeader from "./components/LittleLemonHeader";
 import LittleLemonFooter from "./components/LittleLemonFooter";
 import WelcomeScreen from "./components/WelcomeScreen";
@@ -11,23 +13,38 @@ import LoginScreen from "./components/LoginScreen";
 import Pressable_Menu from "./components/Pressable_Menu";
 import ImageDemo from "./components/ImageDemo";
 import Welcome_useColorScheme from "./components/Welcome_useColorScheme";
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <>
-      <View style={styles.container}>
-        <LittleLemonHeader />
-        <Welcome_useColorScheme />
-        {/* <WelcomeScreen /> */}
-        {/* <MenuItems /> */}
-        {/* <MenuItems_FlatList /> */}
-        {/* <MenuItems_SectionList /> */}
-        {/* <FeedBackForm /> */}
-        {/* <LoginScreen /> */}
-        {/* <LittleLemonFooter /> */}
-        {/* <Pressable_Menu /> */}
-        {/* <ImageDemo /> */}
-      </View>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{ headerStyle: { backgroundColor: "#FBDABB" } }}
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{ title: "Home" }}
+        />
+        {/* <Stack.Screen name="Menu" component={MenuScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <>
+    //   <View style={styles.container}>
+    //     <LittleLemonHeader />
+    //     <Welcome_useColorScheme />
+    //     {/* <WelcomeScreen /> */}
+    //     {/* <MenuItems /> */}
+    //     {/* <MenuItems_FlatList /> */}
+    //     {/* <MenuItems_SectionList /> */}
+    //     {/* <FeedBackForm /> */}
+    //     {/* <LoginScreen /> */}
+    //     {/* <LittleLemonFooter /> */}
+    //     {/* <Pressable_Menu /> */}
+    //     {/* <ImageDemo /> */}
+    //   </View>
+    // </>
   );
 }
 
--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -2,43 +2,35 @@ import { StyleSheet, Text, ScrollView, View, Image } from "react-native";
 
 export default function WelcomeScreen() {
   return (
-    <View style={styles.container}>
-      <ScrollView indicatorStyle="white" style={styles.scrollViewContainer}>
-        <View style={styles.logoText}>
-          <Image
-            resizeMode="cover"
-            style={styles.imageStyle}
-            source={require("C:/Users/Admin/Shilpa/Coursera/Little_Lemon_ReactNative/images/LittleLemonLogo.png")}
-            accessible={true}
-            accessibilityLabel="Little Lemon Logo"
-          />
-          <Text style={styles.headerText}>Little Lemon</Text>
-        </View>
-        {/* <Text style={styles.mainText}>Welcome to Little Lemon</Text> */}
-        <Text style={styles.aboutText}>
-          Little Lemon is a charming neighborhood bistro that serves simple food
-          and classic cocktails in a lively but casual environment. We would
-          love to hear more about your experience with us!
-        </Text>
-      </ScrollView>
-    </View>
+    <ScrollView indicatorStyle="white" style={styles.scrollViewContainer}>
+      <View style={styles.logoText}>
+        <Image
+          resizeMode="cover"
+          style={styles.imageStyle}
+          source={require("C:/Users/Admin/Shilpa/Coursera/Little_Lemon_ReactNative/images/LittleLemonLogo.png")}
+          accessible={true}
+          accessibilityLabel="Little Lemon Logo"
+        />
+        <Text style={styles.headerText}>Little Lemon</Text>
+      </View>
+      {/* <Text style={styles.mainText}>Welcome to Little Lemon</Text> */}
+      <Text style={styles.aboutText}>
+        Little Lemon is a charming neighborhood bistro that serves simple food
+        and classic cocktails in a lively but casual environment. We would love
+        to hear more about your experience with us!
+      </Text>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "#333333" },
-  scrollViewContainer: { flex: 1 },
-  mainText: {
-    padding: 20,
-    fontSize: 30,
-    color: "#EDEFEE",
-    textAlign: "center",
-  },
+  scrollViewContainer: { flex: 1, backgroundColor: "#495E57" },
   aboutText: {
     fontSize: 24,
     padding: 20,
     marginVertical: 8,
-    color: "#EDEFEE",
+    color: "black",
+    // color: "yellow",
     textAlign: "center",
   },
   imageStyle: {
@@ -58,7 +50,8 @@ const styles = StyleSheet.create({
     paddingTop: 30,
     paddingBottom: 10,
     fontSize: 30,
-    color: "#EDEFEE",
+    // color: "#EDEFEE",
+    color: "black",
     textAlign: "center",
   },
 });
